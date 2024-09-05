2f5ca2fc7ce93d8061ea749d92d10c2a44001d06
Your Name <you@example.com>
2024-09-05 10:21:03 +0200

    start stock

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatInputModule } from '@angular/material/input';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { WearhouseModule } from './wearhouse/wearhouse.module';
 import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +24,7 @@ import { HttpClientModule } from '@angular/common/http';
     WearhouseModule,
     MatInputModule,
     BsDropdownModule.forRoot(),
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/wearhouse/wearhouse.module.ts
+++ client/src/app/wearhouse/wearhouse.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { MainwearhouseComponent } from './mainwearhouse/mainwearhouse.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -12,6 +13,7 @@ import { MainwearhouseComponent } from './mainwearhouse/mainwearhouse.component'
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
+    MatCardModule,
   ],
   exports:[MainwearhouseComponent]
 })
