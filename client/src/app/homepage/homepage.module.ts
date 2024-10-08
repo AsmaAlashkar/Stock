bb11ee8755682245229af23d8384dfe196eb0d6f
Asmaa Alashkar <asmaaalashkar02@gmail.com>
2024-10-08 11:03:44 +0300

    some home edits

--- client/src/app/homepage/homepage.module.ts
+++ client/src/app/homepage/homepage.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 
 @NgModule({
@@ -9,7 +10,9 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CardModule,
+    ButtonModule
   ],exports:[
     HomeComponent
   ]
