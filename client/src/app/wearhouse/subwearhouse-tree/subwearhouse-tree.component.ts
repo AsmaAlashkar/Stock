d2addd113f30d99272f0e637dd681ad375463d88
Your Name <you@example.com>
2024-10-08 13:04:33 +0300

    khater-subwarehouse-details

--- client/src/app/wearhouse/subwearhouse-tree/subwearhouse-tree.component.ts
+++ client/src/app/wearhouse/subwearhouse-tree/subwearhouse-tree.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IViewWearhouseItem } from 'src/app/shared/models/IViewWearhouseItem';
 import { WearhouseService } from '../wearhouse.service';
-import { Column, ISubWearhouse } from 'src/app/shared/models/subwearhouse';
 import { TreeNode } from 'primeng/api';
 import { ActivatedRoute } from '@angular/router';
 
@@ -10,88 +9,67 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './subwearhouse-tree.component.html',
   styleUrls: ['./subwearhouse-tree.component.scss']
 })
-export class SubwearhouseTreeComponent implements OnInit{
+export class SubwearhouseTreeComponent implements OnInit {
   @Input() subWearhouses: IViewWearhouseItem[] = [];
-  @Input() mainId!: number;
-  subWearHouses!: ISubWearhouse[];
   subWearhousestree!: TreeNode[];
-  cols!: Column[];
+  cols!: any[];
 
-  constructor(private wearhouseService: WearhouseService,private activeRoute: ActivatedRoute) {}
+  constructor(private wearhouseService: WearhouseService, private activeRoute: ActivatedRoute) {}
 
   ngOnInit() {
-    this.wearhouseService.getSubWearhouseByMainId(this.mainId).subscribe((subWearhouses) => {
-      this.subWearhousestree = this.buildTreeNodes(subWearHouses, this.mainId);
-    });
-
+    this.loadSubWarehouses();
     this.cols = [
-      { field: 'subName', header: 'Name' },
+      { field: 'subName', header: 'Sub Warehouse Name' },
+      { field: 'subAddress', header: 'Sub Address' },
       { field: 'subDescription', header: 'Description' },
-      { field: 'subAddress', header: 'Address' },
       { field: 'subCreatedat', header: 'Created At' },
       { field: 'subUpdatedat', header: 'Updated At' },
+      { field: '', header: 'Actions' }
     ];
   }
 
-  buildTreeNodes(items: ISubWearhouse[],mainId:number): TreeNode[] {
+  loadSubWarehouses() {
+    const mainID = this.activeRoute.snapshot.paramMap.get('id');
+    if (mainID) {
+      const numericItemID = +mainID;
+      this.wearhouseService.getSubWearhouseByMainId(numericItemID).subscribe(data => {
+        this.subWearhouses = data;
+        this.subWearhousestree = this.buildTree(data);
+      });
+    }
+  }
 
+  buildTree(data: IViewWearhouseItem[]): TreeNode[] {
     const map: { [key: number]: TreeNode } = {};
     const roots: TreeNode[] = [];
 
-    items.forEach(item => {
-      map[item.subId] = { data: item, children: [] };
-    });
+    data.forEach(item => {
+      const node: TreeNode = {
+        data: {
+          subName: item.subName,
+          subAddress: item.subAddress,
+          subDescription: item.subDescription,
+          subCreatedat: item.subCreatedat,
+          subUpdatedat: item.subUpdatedat,
+          subId: item.subId
+        },
+        children: []
+      };
+      map[item.subId] = node;
 
-    items.forEach(item => {
       if (item.parentSubWearhouseId) {
-        map[item.parentSubWearhouseId].children!.push(map[item.subId]);
+        const parent = map[item.parentSubWearhouseId];
+        if (parent) {
+          if (!parent.children) {
+            parent.children = [];
+          }
+          parent.children.push(node);
+        }
       } else {
-        roots.push(map[item.subId]);
+        roots.push(node);
       }
     });
 
-     return roots;
+    return roots;
   }
-
-
-    // ngOnInit() {
-  //   this.loadSubWarehouses();
-  // }
-
-  // loadSubWarehouses() {
-  //   const mainID = this.activeRoute.snapshot.paramMap.get('id');
-  //   if (mainID) {
-  //     const numericItemID = +mainID;
-  //   this.wearhouseService.getSubWearhouseByMainId(numericItemID).subscribe(data => {
-  //     this.subWearhouses = data;
-  //     this.subWearhousestree = this.buildTree(data);
-  //   });
-  // }}
-
-  // buildTree(data: IViewWearhouseItem[]): TreeNode[] {
-  //   const map: { [key: number]: TreeNode } = {};
-  //   const roots: TreeNode[] = [];
-
-  //   data.forEach(item => {
-  //     const node: TreeNode = {
-  //       data: item,
-  //       children: [],
-  //     };
-  //     map[item.subId] = node;
-
-  //     if (item.parentSubWearhouseId) {
-  //       const parent = map[item.parentSubWearhouseId];
-  //       if (parent) {
-  //         if (!parent.children) {
-  //           parent.children = [];
-  //         }
-  //         parent.children.push(node);
-  //       }
-  //     } else {
-  //       roots.push(node);
-  //     }
-  //   });
-
-  //   return roots;
-  // }
 }
