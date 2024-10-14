92ac38916f4c5d4241177f3d9909ec5454f1ad51
Moustafa-Ali-Khater <mostafaaliramadan08@gmail.com>
2024-10-14 13:25:08 +0300

    items&permission

--- Repository/ItemRepo/IItemRepository.cs
+++ Repository/ItemRepo/IItemRepository.cs
@@ -12,8 +12,8 @@ namespace Repository.ItemRepo
     public interface IItemRepository
     {
         Task<ItemDetailsResult> GetItems(DTOPaging paging);
-        Task<Item?> GetItemById(int id);
-        Task<List<ItemDetailsDto>> GetItems(DTOPaging paging);
+        //Task<Item?> GetItemById(int id);
+        //Task<List<ItemDetailsDto>> GetItems(DTOPaging paging);
         Task<ItemDetailsDto?> GetItemById(int id);
         Task<List<ItemDetailsDto>> GetItemsByCategoryId(int catId, DTOPaging paging);
         Task<List<ItemDetailsDto>> GetItemsBySubWHId(int subId, DTOPaging paging);
