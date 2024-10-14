57763affe4a3e89efb4a4aa88fad8e7d94c1cfd7
Your Name <you@example.com>
2024-10-14 16:50:24 +0300

    card colors

--- client/src/app/permission/permission-type/permission-type.component.ts
+++ client/src/app/permission/permission-type/permission-type.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PermissionService } from '../permission.service';
+import { IPermissionType } from 'src/app/shared/models/permissiontype';
 
 @Component({
   selector: 'app-permissin-type',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./permission-type.component.scss']
 })
 export class PermissinTypeComponent {
+
+  permissionTypes: IPermissionType[] = [];
+  filteredPermissionTypes: IPermissionType[] = [];
+  filterName: string = ''; // Variable to hold the filter input value
+
+  constructor(private permissionservice: PermissionService) {}
+
+  ngOnInit(): void {
+    this.loadPermissionTypes(); // Call the method on component initialization
+  }
+
+  loadPermissionTypes(): void {
+    this.permissionservice.getPermissionTypes().subscribe(
+      (data: IPermissionType[]) => {
+        this.permissionTypes = data;
+        this.filteredPermissionTypes = data; // Initialize the filtered array with all items
+      },
+      (error) => {
+        console.error('Error fetching permission types', error);
+      }
+    );
+  }
+
+  applyFilter(): void {
+    if (this.filterName.trim() === '') {
+      this.filteredPermissionTypes = this.permissionTypes; // Reset to all items if no filter
+    } else {
+      this.filteredPermissionTypes = this.permissionTypes.filter((permissionType) =>
+        permissionType.perTypeValue.toLowerCase().includes(this.filterName.toLowerCase())
+      );
+    }
+  }
 
 }
--- client/src/app/permission/permission.module.ts
+++ client/src/app/permission/permission.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PermissinTypeComponent } from './permission-type/permission-type.component';
 import { CreatePermissionComponent } from './create-permission/create-permission.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +12,8 @@ import { CreatePermissionComponent } from './create-permission/create-permission
     CreatePermissionComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class PermissionModule { }
