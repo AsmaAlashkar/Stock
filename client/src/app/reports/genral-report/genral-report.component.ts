368157d9147cbcf07fe957ea297dc9c4e71606d1
Your Name <you@example.com>
2024-11-13 11:27:47 +0200

    report-design

--- client/src/app/reports/genral-report/genral-report.component.ts
+++ client/src/app/reports/genral-report/genral-report.component.ts
@@ -10,18 +10,18 @@ export class GenralReportComponent {
   currentDate: string;
 
   constructor() {
-    // const today = new Date();
-    // this.currentDate = today.toLocaleDateString();
-  
     const today = new Date();
-    const arabicFormatter = new Intl.NumberFormat('ar-EG');
+    this.currentDate = today.toLocaleDateString();
+  
+    // const today = new Date();
+    // const arabicFormatter = new Intl.NumberFormat('ar-EG');
 
-    const day = arabicFormatter.format(today.getDate()).padStart(2, '٠');  // Arabic zero
-    const month = arabicFormatter.format(today.getMonth() + 1).padStart(2, '٠');
-    const year = arabicFormatter.format(today.getFullYear()).replace(/٬/g, '');
+    // const day = arabicFormatter.format(today.getDate()).padStart(2, '٠');  // Arabic zero
+    // const month = arabicFormatter.format(today.getMonth() + 1).padStart(2, '٠');
+    // const year = arabicFormatter.format(today.getFullYear()).replace(/٬/g, '');
 
-    // Construct the date string in dd-mm-yy format with Arabic numerals
-    this.currentDate = `${day}-${month}-${year}`;
+    // // Construct the date string in dd-mm-yy format with Arabic numerals
+    // this.currentDate = `${day}-${month}-${year}`;
   }
 
 }
